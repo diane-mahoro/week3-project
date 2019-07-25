e1f6242bfd5fba98c58bd4e4aec8e4eca586cc87
diane-mahoro <www.diammah7@gmail.com>
2019-07-25 12:52:22 +0200

    edition of js file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function() {
+$("#hiden1").click(function() {
+$("#showing1").toggle();
+$("#hiden1").toggle();
+});
 
-    
+ $("#hiden2").click(function() {
+     $("#showing2").toggle();
+     $("#hiden2").toggle();
+ });
+
+ $("#hiden3").click(function() {
+    $(".#howing3").toggle();
+    $("#hiden3").toggle();
+});
+
 });
