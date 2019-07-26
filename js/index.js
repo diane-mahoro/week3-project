8a673192effcef8bb7f04c7369d38fb73f5cc7a8
diane-mahoro <www.diammah7@gmail.com>
2019-07-26 11:30:50 +0200

    change in form

--- js/index.js
+++ js/index.js
@@ -13,6 +13,13 @@ $("#hiden1").toggle();
     $("#showing3").toggle();
     $("#hiden3").toggle();
 });
+$("#im1" || "#im2").hover(function() {
+$("#im1").css("opacity", "0.2");
+$
+},
+function(){
+$("#im1").css("opacity", "1");
+});
 $("form").submit(function(event) {
     alert("thank you very much for contacting us");
 });
