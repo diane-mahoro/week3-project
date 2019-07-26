c40ed96ac6c2b91ad020724e4e372bfabb944512
diane-mahoro <www.diammah7@gmail.com>
2019-07-26 12:43:28 +0200

    change in form

--- js/index.js
+++ js/index.js
@@ -13,13 +13,70 @@ $("#hiden1").toggle();
     $("#showing3").toggle();
     $("#hiden3").toggle();
 });
-$("#im1" || "#im2").hover(function() {
+$("#im1").hover(function() {
 $("#im1").css("opacity", "0.2");
-$
+$("#text1").show();
 },
 function(){
 $("#im1").css("opacity", "1");
+$("#text1").hide();
 });
+$("#im2").hover(function() {
+    $("#im2").css("opacity", "0.2");
+    $("#text2").show();
+    },
+    function(){
+    $("#im2").css("opacity", "1");
+    $("#text2").hide();
+    });
+    $("#im3").hover(function() {
+        $("#im3").css("opacity", "0.2");
+        $("#text3").show();
+        },
+        function(){
+        $("#im3").css("opacity", "1");
+        $("#text3").hide();
+        });
+        $("#im4").hover(function() {
+            $("#im4").css("opacity", "0.2");
+            $("#text4").show();
+            },
+            function(){
+            $("#im4").css("opacity", "1");
+            $("#text4").hide();
+            });
+            $("#im5").hover(function() {
+                $("#im5").css("opacity", "0.2");
+                $("#text5").show();
+                },
+                function(){
+                $("#im5").css("opacity", "1");
+                $("#text5").hide();
+                });
+                $("#im6").hover(function() {
+                    $("#im6").css("opacity", "0.2");
+                    $("#text6").show();
+                    },
+                    function(){
+                    $("#im6").css("opacity", "1");
+                    $("#text6").hide();
+                    });
+                    $("#im7").hover(function() {
+                        $("#im7").css("opacity", "0.2");
+                        $("#text7").show();
+                        },
+                        function(){
+                        $("#im7").css("opacity", "1");
+                        $("#text7").hide();
+                        });
+                        $("#im8").hover(function() {
+                            $("#im8").css("opacity", "0.2");
+                            $("#text8").show();
+                            },
+                            function(){
+                            $("#im8").css("opacity", "1");
+                            $("#text8").hide();
+                            });
 $("form").submit(function(event) {
     alert("thank you very much for contacting us");
 });
